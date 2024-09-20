8b927174cd4db3672600d6d10a1163676e09abf5
Javier <129708957+JavierCisco@users.noreply.github.com>
2024-09-20 13:41:18 -0500

    database for players
    Signed-off-by: Javier <129708957+JavierCisco@users.noreply.github.com>

---
+++ player.sql
@@ -0,0 +1,13 @@
+
+--Create table
+CREATE TABLE players (
+  id INT,
+  codename VARCHAR(30)
+);
+
+--Place first record into table
+INSERT INTO players (id, codename)
+VALUES (1, 'Opus');
+
+Insert into players (1, MJackson);
+
