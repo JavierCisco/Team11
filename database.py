1146ec6d899854611240a3e62f9ba4edb3e99462
Francisco Hernandez <francishs8176@gmail.com>
2024-09-26 17:58:39 -0500

    First attempt at seeing if this works on the virtual machine

---
+++ database.py
@@ -0,0 +1,36 @@
+import psycopg2
+from psycopg2 import sql
+
+# define connection parameters
+connection_params = {
+    'dbname': 'photon',
+    'user': 'student',
+    # commented out, so imma leave em that way
+    'password': 'student',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+try:
+    # connect to postgreSQL
+    conn = psycopg2.connect(**connection_params)
+    cursor = conn.cursor()
+
+    # version stuff
+    cursor.execute("select version();")
+    version = cursor.fetchone()
+    print(f'Connected to - {version}')
+
+    # executing queries
+    cursor.execute('select * from players;')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+except Exception as error:
+    print(f'Error connecting to PostgreSQL database: {error}')
+
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
