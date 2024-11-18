8b6c7de220100b47b53c15d8ff9f88d8c5fb24da
America Cervantes <americacervantes1211@gmail.com>
2024-11-17 21:00:18 -0600

    Update server.py
    updating server file
    
    Signed-off-by: America Cervantes <americacervantes1211@gmail.com>

--- server.py
+++ server.py
@@ -6,58 +6,30 @@ bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
 
-traffic_generator_address = ("127.0.0.1", 7500)
-
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
+# UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
-
-def send_equipment_code(code, address=("127.0.0.1", 7501)):
-    message = str(code).encode('utf-8')
-    UDPServerSocket.sendto(message, address)
-    print(f"Sent equipment code: {code}")
-
-def send_start_signal(address):
-    # send_equipment_code("202", address)
-    message = "202"
-    # UDPServerSocket.sendto(message, address)
-    UDPServerSocket.sendto(message.encode('utf-8'), traffic_generator_address)
-    print("Start signal sent to traffic generator")
-
-def send_stop_signal(address):
-    # send_equipment_code("221", address)
-    message = "221"
-    # UDPServerSocket.sendto(message, address)
-    UDPServerSocket.sendto(message.encode('utf-8'), traffic_generator_address)
-    print("Stop signal sent to traffic generator")
 
 # Listen for incoming datagrams
 try:
     while(True):
     
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-        message = bytesAddressPair[0].decode('utf-8')
+        message = bytesAddressPair[0]
         address = bytesAddressPair[1]
         clientMsg = "Message from Client:{}".format(message)
         clientIP  = "Client IP Address:{}".format(address)
         
-        print(clientMsg)
-        print(clientIP)
-
-        if message == "START":
-            send_start_signal(address)
-        elif message == "STOP":
-            send_stop_signal(address)
-        else:
-            print(f"Unhandled message: {message}")
+        # print(clientMsg)
+        # print(clientIP)
     
         # Sending a reply to client
-        # UDPServerSocket.sendto(bytesToSend, address)
+        UDPServerSocket.sendto(bytesToSend, address)
 finally:
     UDPServerSocket.close()
