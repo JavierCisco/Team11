78788a23cc602a26118205532fe0572146665a5e
America Cervantes <americacervantes1211@gmail.com>
2024-11-17 22:15:15 -0600

    Update server.py
    Signed-off-by: America Cervantes <americacervantes1211@gmail.com>

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 localIP     = "127.0.0.1"
-localPort   = 20001
+localPort   = 7501
 bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
@@ -12,9 +12,18 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Bind to address and ip
-# UDPServerSocket.bind((localIP, localPort))
+UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
+
+def send_test_signal(signal):
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 7500
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(signal.encode('utf-8'), (UDP_IP, UDP_PORT))
+    print(f"Sent signal: {signal}")
+
+send_test_signal("202")
 
 # Listen for incoming datagrams
 try:
