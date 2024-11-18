42ea26819d00db7ab0ad35290f32d4de69412bf6
America Cervantes <americacervantes1211@gmail.com>
2024-11-17 23:19:26 -0600

    Update server.py
    Signed-off-by: America Cervantes <americacervantes1211@gmail.com>

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 localIP     = "127.0.0.1"
-localPort   = 7501
+localPort   = 7500
 bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
@@ -12,33 +12,24 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Bind to address and ip
-# UDPServerSocket.bind((localIP, localPort))
+UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
 
-def send_test_signal(signal):
-    UDP_IP = "127.0.0.1"
-    UDP_PORT = 7500
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(signal.encode('utf-8'), (UDP_IP, UDP_PORT))
-    print(f"Sent signal: {signal}")
-
-send_test_signal("202")
-
 # Listen for incoming datagrams
 try:
-    while(True):
-    
+    while True:
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-        message = bytesAddressPair[0]
+        message = bytesAddressPair[0].decode('utf-8')
         address = bytesAddressPair[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
-        
-        # print(clientMsg)
-        # print(clientIP)
-    
-        # Sending a reply to client
-        UDPServerSocket.sendto(bytesToSend, address)
+
+        print(f"Received message: {message} from {address}")
+
+        if message == '221':
+            print("Game end signal received.")
+            break  # End the server
+        else:
+            # Respond to the client
+            UDPServerSocket.sendto(b"ACK", address)
 finally:
     UDPServerSocket.close()
