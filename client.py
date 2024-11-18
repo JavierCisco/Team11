4460c34fa2b365e33f5b5e613db264e7a8d422fb
America Cervantes <americacervantes1211@gmail.com>
2024-11-17 22:14:28 -0600

    Update client.py
    Signed-off-by: America Cervantes <americacervantes1211@gmail.com>

--- client.py
+++ client.py
@@ -13,12 +13,5 @@ UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 msg = "Message from Server {}".format(msgFromServer[0])
-def send_test_signal(signal):
-    UDP_IP = "127.0.0.1"
-    UDP_PORT = 7500
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(signal.encode('utf-8'), (UDP_IP, UDP_PORT))
-    print(f"Sent signal: {signal}")
 
-send_test_signal("202")
 print(msg)
